519cfee239e62786089a366eaeb7973884e2e338
Dannystu12 <mgdog1@googlemail.com>
2018-09-10 19:18:47 +0100

    initial commit

---
+++ Assets/Fonts/NumberWizard.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour{
+
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
+    int guess;
+
+    // Use this for initialization
+    void Start () {
+        StartGame();
+	}
+
+    void StartGame()
+    {
+        NextGuess();
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+    }
+
+    public void OnPressHigher()
+    {
+        min = guess + 1;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess - 1;
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        if(min <= max)
+         guess = Random.Range(min, max + 1);
+        guessText.text = guess.ToString();
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int numScenes = SceneManager.sceneCountInBuildSettings;
+        int nextSceneIndex = currentSceneIndex == (numScenes - 1) ? 0 : currentSceneIndex + 1;
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}  
